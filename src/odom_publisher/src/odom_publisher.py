4fe09dc615f2753a9f2b30b5eb9072d635bc2368
MTN <mtyranskin@gmail.com>
2023-03-16 09:16:36 +0100

    Odometry working

---
+++ src/odom_publisher/src/odom_publisher.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Header
+from gazebo_msgs.srv import GetModelState, GetModelStateRequest
+
+rospy.init_node('odom_pub')
+
+odom_pub=rospy.Publisher ('/odom', Odometry, queue_size=1)
+
+rospy.wait_for_service ('/gazebo/get_model_state')
+get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+
+odom=Odometry()
+header = Header()
+header.frame_id='/odom'
+
+model = GetModelStateRequest()
+model.model_name='rr_robot'
+
+r = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+    result = get_model_srv(model)
+
+    odom.pose.pose = result.pose
+    odom.twist.twist = result.twist
+
+    header.stamp = rospy.Time.now()
+    odom.header = header
+
+    odom_pub.publish (odom)
+
+    r.sleep()
