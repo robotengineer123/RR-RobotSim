62494df4766635075f85bfd03ec98df843ffb9f1
Alexander Stoustrup <alexanderstoustrup@outlook.dk>
2023-03-28 09:16:26 +0200

    Changed winch plugin to work with winch joint

--- src/winch_force_plugin/include/winch_force_plugin/winch.h
+++ src/winch_force_plugin/include/winch_force_plugin/winch.h
@@ -12,51 +12,43 @@ namespace WinchPlugin
     class Winch
     {
     public:
-        Winch(ros::NodeHandle *nh_ptr, 
-            gazebo::physics::LinkPtr rope_link, 
-            ignition::math::Vector3d fixed_pos, 
-            std::string motor_state_topic, 
-            std::string radius_topic, 
-            int prismatic_axis, 
+        Winch(ros::NodeHandle *nh_ptr,
+            gazebo::physics::LinkPtr winch_link,
+            ignition::math::Vector3d fixed_pos,
+            std::string radius_topic,
+            int winch_axis,
             bool rot_dir_switch=false);
         
         void SetStrainCurvePolyCoeffs(ignition::math::Vector3d poly_coeffs);
-        void UpdateState();
-        double GetSimPosition();
-        double GetRealPosition();
-        const ignition::math::Vector3d& GetRopeDir();
-        void ApplyRopeTension(double tension);
+        void UpdateRopeTension();
 
-        void EffRadiusCallback(const std_msgs::Float64ConstPtr &msg);
-        void MotorCallback(const rr_msgs::MotorStateConstPtr &msg);
 
     private:
+        void EffRadiusCallback(std_msgs::Float64ConstPtr msg);
+        void UpdateLenghts();
         double RopeStiffness(double strain);
-        void UpdateStiffness();
+        double UnwindStep();
+        double PosAngle(double angle);
 
         ros::NodeHandle* nh;
 
         ros::Subscriber eff_r_sub;
-        ros::Subscriber motor_sub;
 
         double eff_radius_ = 0.2;
-        double encoder_pos_ = 0;
-        double prev_encoder_pos_ = 0;
+        double prev_rot = 0;
         
         int rot_dir_;
         bool first_read = true;
         
         ignition::math::Vector3d rope_dir_;
 
-        double real_rope_len_ = 0;
-        double sim_rope_len_ = 0;
-        double sim_vel = 0;
-        double real_vel = 0;
+        double rope_len_ = 0;
+        double dist_to_fixed_ = 0;
 
-        gazebo::physics::LinkPtr rope_link_;
-        gazebo::physics::JointPtr joint_;
+        gazebo::physics::LinkPtr winch_link_;
+        gazebo::physics::JointPtr winch_joint_;
 
-        int axis_ = 1;
+        int winch_axis_ = 1;
         ignition::math::Vector3d fixed_pos_{10, 0.0, 0.0};
         ignition::math::Vector3d poly_coeffs_{848.64823567, 188.91463659,  13.92308499};
     };
--- src/winch_force_plugin/src/winch_force_plugin.cpp
+++ src/winch_force_plugin/src/winch_force_plugin.cpp
@@ -19,15 +19,9 @@ namespace gazebo
         {
             model_ = _parent;
             sdf_ = _sdf;
-
-            for(auto& link : model_->GetLinks()) 
-                tot_mass_+=link->GetInertial()->Mass();
-
-            gravity_dir_ = model_->GetWorld()->Gravity().Normalize();
             
             StartNode();
             InitWinches();
-            InitPIDs();
 
             ros::spinOnce();
 
@@ -44,47 +38,23 @@ namespace gazebo
 
             right_winch_ptr_ = std::make_unique<WinchPlugin::Winch>(
                 nh_.get(),
-                model_->GetLink(LoadParam<std::string>("right_rope_link")),
+                model_->GetLink(LoadParam<std::string>("right_winch_link")),
                 LoadParam<ignition::math::Vector3d>("right_fixed_pos"),
-                LoadParam<std::string>("right_motor_topic"),
                 LoadParam<std::string>("right_radius_topic"),
-                LoadParam<int>("right_prismatic_axis"),
+                LoadParam<int>("right_winch_axis"),
                 LoadParam<bool>("right_rot_dir_switch")
                 );
             right_winch_ptr_->SetStrainCurvePolyCoeffs(poly_coeffs);
 
             left_winch_ptr_ = std::make_unique<WinchPlugin::Winch>(
                 nh_.get(),
-                model_->GetLink(LoadParam<std::string>("right_rope_link")),
+                model_->GetLink(LoadParam<std::string>("left_winch_link")),
                 LoadParam<ignition::math::Vector3d>("left_fixed_pos"),
-                LoadParam<std::string>("left_motor_topic"),
                 LoadParam<std::string>("left_radius_topic"),
-                LoadParam<int>("left_prismatic_axis"),
+                LoadParam<int>("left_winch_axis"),
                 LoadParam<bool>("left_rot_dir_switch")
                 );
             left_winch_ptr_->SetStrainCurvePolyCoeffs(poly_coeffs);
-        }
-
-
-        void InitPIDs()
-        {
-            double kp = LoadParam<double>("kp");
-            double kd = LoadParam<double>("kd");
-            double ki = LoadParam<double>("ki");
-
-            right_pid_ = WinchPlugin::PID(
-                    kp,
-                    kd,
-                    ki,
-                    std::bind(&WinchPlugin::Winch::GetRealPosition, right_winch_ptr_.get())
-                    );
-
-            left_pid_ = WinchPlugin::PID(
-                    kp,
-                    kd,
-                    ki,
-                    std::bind(&WinchPlugin::Winch::GetRealPosition, left_winch_ptr_.get())
-                    );
         }
 
         void StartNode()
@@ -113,36 +83,11 @@ namespace gazebo
         void OnUpdate()
         {
             ros::spinOnce();
-            ApplyForce(*right_winch_ptr_, right_pid_);
-            ApplyForce(*left_winch_ptr_, left_pid_);
+            right_winch_ptr_->UpdateRopeTension();
+            left_winch_ptr_->UpdateRopeTension();
         }
 
     private:
-
-        void ApplyForce(WinchPlugin::Winch& winch, WinchPlugin::PID& pid)
-        {
-            winch.UpdateState();
-            double pid_output = pid.Compute(winch.GetSimPosition());
-            auto static_tension = StaticRopeTension(winch.GetRopeDir());
-            double rope_tension = pid_output + static_tension;
-            
-            if (rope_tension < 0)
-            {
-                if (pid.integral < - static_tension) 
-                    pid.integral = - static_tension;
-
-                return;
-            }
-            winch.ApplyRopeTension(rope_tension);
-        }
-
-        double StaticRopeTension(const ignition::math::Vector3d& rope_dir) const
-        {
-            double cos_angle = std::abs(rope_dir.Dot(gravity_dir_));
-            double static_force = tot_mass_*9.82/cos_angle/2;
-            return static_force;
-        }
-
 
         physics::ModelPtr model_;
         sdf::ElementPtr sdf_;
@@ -150,13 +95,8 @@ namespace gazebo
 
         std::unique_ptr<ros::NodeHandle> nh_;
 
-        double tot_mass_ = 0;
-        ignition::math::Vector3d gravity_dir_;
-
         std::unique_ptr<WinchPlugin::Winch> right_winch_ptr_;
         std::unique_ptr<WinchPlugin::Winch> left_winch_ptr_;
-        WinchPlugin::PID right_pid_;
-        WinchPlugin::PID left_pid_;
     };
 
     // Register this plugin with the simulator
