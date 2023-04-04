1011faf3403dd9cc11e32822e9bc7c16e1bb5289
MTN <mtyranskin@gmail.com>
2023-04-04 18:14:05 +0200

    Verific ctrl tuned, cfm 0.0001->0.001: stability

--- src/verification_controller/src/verification_controller.cpp
+++ src/verification_controller/src/verification_controller.cpp
@@ -24,10 +24,12 @@ bool VerificationController::init(hardware_interface::RobotHW* robot_hw,
     r_steer_jh_ = pos_joint_if->getHandle(r_steer_name_);
     l_steer_jh_ = pos_joint_if->getHandle(l_steer_name_);
 
-    odom_sub_ = controller_nh.subscribe("odom", 1, &VerificationController::OdomCallback, this);
+    odom_sub_ = controller_nh.subscribe("/odom", 1, &VerificationController::OdomCallback, this);
+    yaw_pub = controller_nh.advertise<std_msgs::Float64>("/yaw", 50);
+    velX_pub = controller_nh.advertise<std_msgs::Float64>("/velX", 50);
 
-    pidV.initPid(6.0, 1.0, 2.0, 0.3, -0.3);
-    pidY.initPid(6.0, 1.0, 2.0, 0.3, -0.3);
+    pidV.initPid(40.0, 25.0, 5.5, 2.5, -2.5);
+    pidY.initPid(4.5, 1.5, 1.5, 0.3, -0.3);
 
     return true;
 }
@@ -46,25 +48,23 @@ void VerificationController::Brake()
 void VerificationController::starting(const ros::Time& time)
 {
     Brake();
-    odom_buf_pose_.initRT(odom_pose);
-    odom_buf_twist_.initRT(odom_twist);
+    odom_buf_.initRT(odom_cmd);
 }
 
 void VerificationController::update(const ros::Time& time, const ros::Duration& period)
 {
-    odom_pose = *(odom_buf_pose_.readFromRT());
-    odom_twist = *(odom_buf_twist_.readFromRT());
+    odom = *(odom_buf_.readFromRT());
 
     // get yaw and velocity
     tf::Quaternion q(
-        odom_pose.pose.orientation.x,
-        odom_pose.pose.orientation.y,
-        odom_pose.pose.orientation.z,
-        odom_pose.pose.orientation.w);
+        odom.pose.pose.orientation.x,
+        odom.pose.pose.orientation.y,
+        odom.pose.pose.orientation.z,
+        odom.pose.pose.orientation.w);
     tf::Matrix3x3 m(q);
     m.getRPY(currentRoll, currentPitch, currentYaw);
     
-    currentVel = odom_twist.twist.linear.x;
+    currentVel = odom.twist.twist.linear.x;
 
     // PID
     double velocity_r = pidV.updatePid(currentVel - vel_desi_, time - last_time);
@@ -75,6 +75,12 @@ void VerificationController::update(const ros::Time& time, const ros::Duration& 
     double pid_combined_l = velocity_r - yaw;
     
     last_time = time;
+
+    // Publish scanner message
+    currentYawMsg.data = currentYaw;
+    yaw_pub.publish(currentYawMsg);
+    currentVelMsg.data = currentVel;
+    velX_pub.publish(currentVelMsg);
 
     // Set commands
     r_drive_jh_.setCommand(pid_combined_r);
@@ -90,8 +96,7 @@ void VerificationController::stopping(const ros::Time& time)
 
 void VerificationController::OdomCallback(nav_msgs::OdometryConstPtr cmd)
 {
-    odom_pose = cmd->pose;
-    odom_twist = cmd->twist;
-    odom_buf_pose_.writeFromNonRT(odom_pose);
-    odom_buf_twist_.writeFromNonRT(odom_twist);
+    odom_cmd.pose.pose = cmd->pose.pose;
+    odom_cmd.twist.twist = cmd->twist.twist;
+    odom_buf_.writeFromNonRT(odom_cmd);
 }
