5c1f3312f2514b87f4c9922d6cc6fdf42d0497bc
Alexander Stoustrup <alexanderstoustrup@outlook.dk>
2023-05-25 21:45:11 +0200

    Twist changed to vehicle frame as pr nav msg req.

--- src/odom_publisher/src/odom_publisher.py
+++ src/odom_publisher/src/odom_publisher.py
@@ -3,21 +3,27 @@
 import rospy
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
+from geometry_msgs.msg import Vector3Stamped, Twist
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 import tf
 
-br = tf.TransformBroadcaster()
-
 rospy.init_node('odom_pub')
 
-odom_pub=rospy.Publisher ('/odom', Odometry, queue_size=1)
+br = tf.TransformBroadcaster()
+tf_sub = tf.TransformListener()
+
+
+# odom_pub=rospy.Publisher ('/odom', Odometry, queue_size=1)
 
 rospy.wait_for_service ('/gazebo/get_model_state')
 get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 
 odom=Odometry()
 header = Header()
+twist = Twist()
 header.frame_id='/odom'
+odom.child_frame_id = "/dummy"
+
 
 model = GetModelStateRequest()
 model.model_name='rr_robot'
@@ -27,18 +33,32 @@ r = rospy.Rate(50)
 while not rospy.is_shutdown():
     result = get_model_srv(model)
 
-    odom.pose.pose = result.pose
-    odom.twist.twist = result.twist
-
     header.stamp = rospy.Time.now()
-    odom.header = header
-
-    odom_pub.publish (odom)
+    odom.pose.pose = result.pose
 
     br.sendTransform((result.pose.position.x, result.pose.position.y, result.pose.position.z),
-                    (result.pose.orientation.x, result.pose.orientation.y, result.pose.orientation.z, result.pose.orientation.w),
-                    rospy.Time.now(),
-                    "dummy",
-                    "odom")    
+                (result.pose.orientation.x, result.pose.orientation.y, result.pose.orientation.z, result.pose.orientation.w),
+                rospy.Time.now(),
+                "dummy",
+                "odom")    
+
+    linear_stamped = Vector3Stamped(header=header, vector=result.twist.linear)
+    angular_stamped = Vector3Stamped(header=header, vector=result.twist.angular)
+
+
+    try:
+        twist.linear = tf_sub.transformVector3("dummy", linear_stamped).vector
+        twist.angular = tf_sub.transformVector3("dummy", angular_stamped).vector
+    except(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        r.sleep()
+        continue
+
+    odom.twist.twist = twist
+
+    odom.header = header
+
+    # odom_pub.publish(odom)
 
     r.sleep()
+
+
